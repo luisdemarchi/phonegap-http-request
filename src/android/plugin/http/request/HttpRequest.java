ac76fb640ca38f082604a63ad35c69891fa556a4
Masashi Katsumata <wf9a5m75@gmail.com>
2015-01-25 23:01:59 -0800

    Update HttpRequest.java
    http://stackoverflow.com/a/9181171/697856

--- src/android/plugin/http/request/HttpRequest.java
+++ src/android/plugin/http/request/HttpRequest.java
@@ -52,8 +52,7 @@ public class HttpRequest extends CordovaPlugin {
       Map<String, String> options = new HashMap<String, String>();
       if (args.length() > 2 && !args.get(2).equals(null)) {
         params = args.getJSONObject(2);
-        @SuppressWarnings("unchecked")
-        Iterator<String> iterator = params.keys();
+        Iterator<?> iterator = params.keys();
         String key;
         while(iterator.hasNext()){
           key = String.valueOf(iterator.next());
